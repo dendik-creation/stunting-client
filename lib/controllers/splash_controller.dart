f51dfa733ef629b2c80ae87b7fa1e64b9e6c9735
dendik-creation <setiawandendik0205@gmail.com>
2024-08-18 12:37:14 +0700

    feat: Add Logout Method fix : Fix dynamic userId, fix shared_preference, fix initial if has user auth

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -1,10 +1,39 @@
+// ignore_for_file: use_build_context_synchronously
+import 'dart:developer';
+
+import 'package:client/utils/auth_user.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 class SplashController {
+  void hasUserAuth(BuildContext context) async {
+    final keluargaAuth = await AuthUser.getData('keluarga_auth');
+    final userAuth = await AuthUser.getData('user_auth');
+
+    if (keluargaAuth != null) {
+      keluargaHome(context);
+    } else if (userAuth != null) {
+      userHome(context);
+    } else {
+      goOnboarding(context);
+    }
+  }
+
   void goOnboarding(BuildContext context) {
     Timer(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacementNamed('/onboarding');
+    });
+  }
+
+  void keluargaHome(BuildContext context) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/home-keluarga');
+    });
+  }
+
+  void userHome(BuildContext context) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/home-user');
     });
   }
 }
